4f645de7b630bcca5e83537c9ac2e8e9ff038620
hushichang <hushichang@zhihu.com>
2019-11-07 07:41:00 +0800

    读写锁的例子

---
+++ anything/rwmutex.go
@@ -0,0 +1,79 @@
+package anything
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+)
+
+/**
+* @Author: 胡大海
+* @Date: 2019-10-26 09:52
+* A programmer who subconsciously views himself as an artist will enjoy what he does and will do it better ​
+ */
+
+// 代码地址：https://medium.com/golangspec/sync-rwmutex-ca6c6c3208a0
+
+// 如何表示内部的状态呢？
+// RLock 和 Lock 锁住的到底是什么呢？
+// 还是开发人员自己定义这个锁住的东西？
+// 如何定义一轮呢？？
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func sleep() {
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+}
+
+func reader(c chan int, m *sync.RWMutex, wg *sync.WaitGroup) {
+	sleep()
+	m.RLock()
+	c <- 1
+	sleep()
+	c <- -1
+	m.RUnlock()
+	wg.Done()
+}
+
+func writer(c chan int, m *sync.RWMutex, wg *sync.WaitGroup) {
+	sleep()
+	m.Lock()
+	c <- 1
+	sleep()
+	c <- -1
+	m.Unlock()
+	wg.Done()
+}
+
+func RWLockExp() {
+	var m sync.RWMutex
+	var rs, ws int
+	rsCh := make(chan int)
+	wsCh := make(chan int)
+	// 这个goroutine会不会泄露呢
+	go func() {
+		for {
+			select {
+			case n := <-rsCh:
+				rs += n
+			case n := <-wsCh:
+				ws += n
+			}
+			fmt.Printf("%s%s\n", strings.Repeat("R", rs), strings.Repeat("W", ws))
+		}
+	}()
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go reader(rsCh, &m, &wg)
+	}
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go writer(wsCh, &m, &wg)
+	}
+	wg.Wait()
+}
