25bdca52738e774933b3a9ff8f543755030246a4
hushichang <hushichang@zhihu.com>
2020-06-13 15:32:16 +0800

    chanel close 学习

--- anything/closingchannel.go
+++ anything/closingchannel.go
@@ -12,31 +12,22 @@ func ClosingChannelExp() {
 	jobs := make(chan int, 5)
 	done := make(chan bool)
 	go func() {
-		//for ele := range jobs {
-		//	fmt.Println("receive job ", ele)
-		//}
-		//done <- true
 		for {
-			// 感觉换成ok更合适，ok为true的时候，表示有值出来，
-			// 否则jobs就被close了，并且为空
-			j, ok := <-jobs
-			fmt.Println("status is ", j, ok)
+			job, ok := <- jobs
 			if ok {
-				fmt.Println("received job ", j)
+				fmt.Println("received job ", job)
 			} else {
-				fmt.Println("received all jobs")
+				fmt.Println("jobs channel closed")
 				done <- true
-				break
+				return
 			}
 		}
 	}()
-	for i:=0; i<3; i++ {
-		jobs <- i+1
-		fmt.Println("sent job ", i+1)
+
+	for i := 0; i < 3; i++ {
+		jobs <- i
+		fmt.Println("send job ", i)
 	}
-	//close(jobs)
-	fmt.Println("sent all jobs")
-	// 由于是没有长度的channel，所以接受的时候，如果channel中没有值，就会一直在这阻塞
-	// 直到channel中准备传递值为止， 这样也就是相当于完成了等待goroutine执行完成的过程
+	close(jobs)
 	<- done
 }
