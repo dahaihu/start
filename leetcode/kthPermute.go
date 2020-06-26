0cb16d4f653190a07f882f3fcced21e09dd4337b
hushichang <hushichang@zhihu.com>
2020-06-26 21:05:31 +0800

    调整实现的细节

--- leetcode/kthPermute.go
+++ leetcode/kthPermute.go
@@ -38,42 +38,35 @@ import (
 
 func GetPermutation(n int, k int) string {
 	res := bytes.Buffer{}
+	items := make([]int, n)
 	mark := make([]int, n)
 	mark[0] = 1
+	items[0] = 1
 	for i := 1; i < n; i++ {
 		mark[i] = mark[i-1] * i
-	}
-	items := make([]int, n)
-	for i := 0; i < n; i++ {
 		items[i] = i + 1
 	}
-	var idx, item int
+	var idx int
 	for true {
-		idx, k = k/mark[len(items)-1], k%mark[len(items)-1]
+		idx, k = k / mark[len(items) - 1], k % mark[len(items) - 1]
 		if k == 0 {
-			items, item = popIdxItem(items, idx-1)
-			res.WriteString(strconv.Itoa(item))
-			for i := len(items) - 1; i >= 0; i-- {
+			res.WriteString(strconv.Itoa(items[idx - 1]))
+			items = popKItem(items, idx-1)
+			for i := len(items) - 1; i >=0; i-- {
 				res.WriteString(strconv.Itoa(items[i]))
 			}
 			break
 		}
-		items, item = popIdxItem(items, idx)
-		res.WriteString(strconv.Itoa(item))
-		if k == 1 {
-			for i := 0; i < len(items); i++ {
-				res.WriteString(strconv.Itoa(items[i]))
-			}
-			break
-		}
-
+		res.WriteString(strconv.Itoa(items[idx]))
+		items = popKItem(items, idx)
 	}
 	return res.String()
 }
-func popIdxItem(items []int, idx int) ([]int, int) {
-	item := items[idx]
-	newItems := make([]int, idx)
-	copy(newItems, items[:idx])
-	newItems = append(newItems, items[idx+1:]...)
-	return newItems, item
+
+
+func popKItem(items []int, idx int) []int {
+	res := make([]int, len(items) - 1)
+	copy(res[:idx], items[:idx])
+	copy(res[idx:], items[idx+1:])
+	return res
 }
