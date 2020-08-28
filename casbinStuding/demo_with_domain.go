662ce12cf5524736eef3b7859b665467936fe79f
hushichang <hscxrzs@gmail.com>
2020-08-28 08:49:22 +0800

    casbin 带有 domain

--- casbinStuding/demo_with_domain.go
+++ casbinStuding/demo_with_domain.go
@@ -9,10 +9,10 @@ func demoWithDomain() {
 	e, err := casbin.NewEnforcer("auth_model_with_domain.conf", "policy_with_domain.csv")
 	fmt.Println("initiate err is ", err)
 	if err == nil {
-		ok ,_ := e.Enforce("hushichang", "zhihu", "/login", "admin")
+		ok ,_ := e.Enforce("hushichang", "/login", "admin", "zhihu")
 		fmt.Println("ok is ", ok)
 	}
-	fmt.Println(e.GetRolesForUser("hushichang", "zhihu"))
+	fmt.Println(e.GetImplicitRolesForUser("hushichang", "zhihu"))
 	fmt.Println(e.GetImplicitPermissionsForUser("hushichang", "zhihu"))
 	//fmt.Println(e.GetRolesForUser(""))
 }
