b70c713faeee12b44b7b86b96e375cbc891e046a
hushichang <hushichang@zhihu.com>
2020-10-03 08:51:21 +0800

    非常有意思的一个内容

--- contextStuding/contextNotTerminateThemselfes.go
+++ contextStuding/contextNotTerminateThemselfes.go
@@ -14,27 +14,25 @@ import (
  */
 
 func inner(ctx context.Context, res chan int) {
-	time.Sleep(time.Duration(rand.Int63n(300)) * time.Millisecond)
+	time.Sleep(time.Duration(rand.Int63n(600)) * time.Millisecond)
 	select {
 	case <- ctx.Done():
 		fmt.Println("inner timeout")
 	case res <- 100:
-		fmt.Println("send to channel")
+		fmt.Println("inner done")
 	}
 }
 
 func outer() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(150 * time.Millisecond))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(300)*time.Millisecond)
 	defer cancel()
 	res := make(chan int)
 	go inner(ctx, res)
 	select {
 	case <-ctx.Done():
-		fmt.Println("out timeout")
-	case val := <- res:
-		fmt.Println("received value is ", val)
+		fmt.Println("outer done")
+	case val := <-res:
+		fmt.Println("outer finished, result is ", val)
 	}
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Duration(300) * time.Millisecond)
 }
-
-
