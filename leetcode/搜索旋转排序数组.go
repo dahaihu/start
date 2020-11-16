819efb91df5b503fe28db9b3d5bb42accd34878c
hushichang <hscxrzs@gmail.com>
2020-11-16 14:54:10 +0800

    搜索旋转排序数组，最后一次做了吧！！！

--- leetcode/搜索旋转排序数组.go
+++ leetcode/搜索旋转排序数组.go
@@ -1,24 +1,23 @@
 package leetcode
 
 func search(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-	for left <= right {
-		// maybe mid == left, then recursive
-		mid := (right-left)/2 + left
+	left, right := 0, len(nums)
+	for left < right {
+		mid := (right - left)/2 + left
 		if nums[mid] == target {
 			return mid
 		}
-		if nums[0] <= nums[mid] {
-			if target <= nums[mid] && target >= nums[left] {
-				right = mid - 1
+		if nums[mid] >= nums[0] {
+			if target < nums[mid] && target >= nums[0] {
+				right = mid
 			} else {
 				left = mid + 1
 			}
 		} else {
-			if target <= nums[right] && target >= nums[mid] {
+			if target <= nums[right-1] && target > nums[mid] {
 				left = mid + 1
 			} else {
-				right = mid - 1
+				right = mid
 			}
 		}
 	}
