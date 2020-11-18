89e31950ed10431b0c5e59c07aaf004dec4ba693
hushichang <hushichang@zhihu.com>
2020-11-18 17:53:11 +0800

    添加测例以及数据相关

--- leetcode/有序矩阵中是否存在数字K_test.go
+++ leetcode/有序矩阵中是否存在数字K_test.go
@@ -2,7 +2,9 @@ package leetcode
 
 import (
 	"fmt"
+	"sync/atomic"
 	"testing"
+	"unsafe"
 )
 
 /**
@@ -11,7 +13,21 @@ import (
 * A programmer who subconsciously views himself as an artist will enjoy what he does and will do it better ​
  */
 
+type Test struct {
+	age int
+}
+
 
 func TestExistValInMultiArray(t *testing.T) {
+	test := Test{age: 10}
+	fmt.Println((&test).age)
+	fmt.Println(&test.age)
+	a := 10
+	b := 20
+	addr := unsafe.Pointer(&a)
+	//fmt.Println(&a == &b)
+	bddr := unsafe.Pointer(&b)
+	fmt.Println("changed ", atomic.CompareAndSwapPointer(&addr, addr, bddr))
+	fmt.Println(*(*int)(addr))
 	fmt.Println(existValInMultiArray([][]int{{1, 2, 3}, {2, 3, 4}, {3, 4, 5}}, -1))
 }
