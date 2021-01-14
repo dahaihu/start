b8c4c7918900bab88fc69b8a7cfc72d76955ffce
hushichang <hscxrzs@gmail.com>
2021-01-14 08:57:31 +0800

    feat: add reflect call func

---
+++ reflect/caller.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func call(f interface{}, args []interface{}) {
+	in := make([]reflect.Value, len(args))
+	for i := range args {
+		in[i] = reflect.ValueOf(args[i])
+	}
+	reflect.ValueOf(f).Call(in)
+}
+
+func greet(a int64, b string) {
+	fmt.Printf("a is %v, b is %v\n", a, b)
+}
+
+
+func main() {
+	call(greet, []interface{}{int64(10), "world"})
+}
