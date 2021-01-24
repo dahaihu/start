768c183df25c1ade2040f956b09821024ec31991
hushichang <hscxrzs@gmail.com>
2021-01-24 21:34:54 +0800

    feat: yml studying

--- yml/yml.go
+++ yml/yml.go
@@ -6,23 +6,29 @@ import (
 	"log"
 )
 
+type mysql struct {
+	User     string `yaml:"user"`
+	Host     string `yaml:"host"`
+	Password string `yaml:"password"`
+	Port     string `yaml:"port"`
+	Name     string `yaml:"name"`
+}
+
+type cache struct {
+	Enable bool     `yaml:"enable"`
+	List   []string `yaml:"list"`
+}
+
+type person struct {
+	Name  string   `yaml:"name"`
+	Age   int64    `yaml:"age"`
+	Names []string `yaml:"names"`
+}
+
 type Yaml struct {
-	Mysql struct {
-		User     string `yaml:"user"`
-		Host     string `yaml:"host"`
-		Password string `yaml:"password"`
-		Port     string `yaml:"port"`
-		Name     string `yaml:"name"`
-	}
-	Cache struct {
-		Enable bool     `yaml:"enable"`
-		List   []string `yaml:"list,flow"`
-	}
-	Person []struct {
-		Name  string   `yaml:"name"`
-		Age   int64    `yaml:"age"`
-		Names []string `yaml:"names"`
-	} `yaml:"persons"`
+	Mysql   mysql    `yaml:"mysql"`
+	Cache   cache    `yaml:"cache"`
+	Persons []person `yaml:"persons"`
 }
 
 func main() {
