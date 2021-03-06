eea1e46dc54a1974b68a020adfd02f1c0d9e9085
hushichang <hsccrzs@gmail.com>
2021-03-05 17:59:51 -0800

    feat: review coin change

--- leetcode/coin_change.go
+++ leetcode/coin_change.go
@@ -10,22 +10,23 @@ func coinChange(coins []int, amount int) int {
 	if amount == 0 {
 		return 0
 	}
-	sort.Slice(coins, func(i, j int) bool { return coins[i] <= coins[j] })
 	mark := make([]int, amount+1)
-	for curAmount := coins[0]; curAmount <= amount; curAmount++ {
-		min := math.MaxInt64
+	sort.Slice(coins, func(i, j int) bool {return coins[i] <= coins[j]})
+	var tmpMin int
+	for curAmount := coins[0]; curAmount < amount+1; curAmount++ {
+		tmpMin = math.MaxInt64
 		for _, coin := range coins {
 			if curAmount < coin {
 				break
 			}
-			if (curAmount == coin || mark[curAmount-coin] > 0) && mark[curAmount-coin] < min {
-				min = mark[curAmount-coin]
+			if (curAmount == coin || mark[curAmount-coin] != 0) &&
+				mark[curAmount-coin] < tmpMin {
+				tmpMin = mark[curAmount-coin]
 			}
 		}
-		if min == math.MaxInt64 {
-			continue
+		if tmpMin != math.MaxInt64 {
+			mark[curAmount] = tmpMin + 1
 		}
-		mark[curAmount] = min + 1
 	}
 	if mark[amount] == 0 {
 		return -1
