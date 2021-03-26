0efd445d0e98b99a76adde7d6121df27687eeccd
hushichang <hsccrzs@gmail.com>
2021-03-26 07:37:05 -0700

    feat: review coin change

--- leetcode/coin_change.go
+++ leetcode/coin_change.go
@@ -1,69 +1,66 @@
 package leetcode
 
 import (
-	"fmt"
 	"math"
 	"sort"
 )
 
 // how to initiate
 func coinChange(coins []int, amount int) int {
-	if amount == 0 {
-		return 0
-	}
 	mark := make([]int, amount+1)
-	sort.Slice(coins, func(i, j int) bool { return coins[i] <= coins[j] })
-	var tmpMin int
-	for curAmount := coins[0]; curAmount < amount+1; curAmount++ {
-		tmpMin = math.MaxInt64
-		for _, coin := range coins {
-			if curAmount < coin {
+	for idx := 1; idx <= amount; idx++ {
+		mark[idx] = -1
+	}
+	sort.Slice(coins, func(i, j int) bool {return coins[i] <= coins[j]})
+	for curAmount := coins[0]; curAmount <= amount; curAmount++ {
+		minCnt := math.MaxInt64
+		for _, coin :=range coins {
+			if coin > curAmount {
 				break
 			}
-			if (curAmount == coin || mark[curAmount-coin] != 0) &&
-				mark[curAmount-coin] < tmpMin {
-				tmpMin = mark[curAmount-coin]
+			if tmp := mark[curAmount-coin]; tmp != -1 && tmp < minCnt {
+				minCnt = tmp
 			}
 		}
-		if tmpMin != math.MaxInt64 {
-			mark[curAmount] = tmpMin + 1
+		if minCnt != math.MaxInt64 {
+			mark[curAmount] = minCnt + 1
 		}
-	}
-	if mark[amount] == 0 {
-		return -1
 	}
 	return mark[amount]
 }
 
 func coinChangeFromTop(coins []int, amount int) int {
+	const NOTPROCESSED = -2
+	const NOTREACHABLE = -1
 	mark := make([]int, amount+1)
 	for idx := 1; idx <= amount; idx++ {
-		mark[idx] = -1
+		mark[idx] = NOTPROCESSED
 	}
 	sort.Slice(coins, func(i, j int) bool { return coins[i] <= coins[j] })
 	var dp func(int) int
 	dp = func(curAmount int) int {
-		if mark[curAmount] >= 0 {
+		if mark[curAmount] == NOTREACHABLE {
+			return math.MaxInt64
+		} else if mark[curAmount] != NOTPROCESSED {
 			return mark[curAmount]
 		}
 		min := math.MaxInt64
 		for _, coin := range coins {
 			if coin > curAmount {
-				continue
+				break
 			}
 			if tmp := dp(curAmount - coin); tmp < min && tmp >= 0 {
 				min = tmp
 			}
 		}
 		if min == math.MaxInt64 {
-			mark[curAmount] = -1
+			mark[curAmount] = NOTREACHABLE
 		} else {
 			mark[curAmount] = min + 1
 		}
 		return mark[curAmount]
 	}
 	tmp := dp(amount)
-	fmt.Println("mark is ", mark)
 	return tmp
 }
 
--- leetcode/coin_change_test.go
+++ leetcode/coin_change_test.go
@@ -6,5 +6,22 @@ import (
 )
 
 func Test_coinChange(t *testing.T) {
-	fmt.Println(coinChange([]int{1}, 10))
+	fmt.Println(coinChangeUsingDfs([]int{186, 419, 83, 408}, 6249))
+	//fmt.Println(coinChangeUsingDfs([]int{1, 3, 4}, 10))
+}
+
+func Test_ArrayPointer(t *testing.T) {
+	arr := make([]*int, 10)
+	for idx, ele := range arr {
+		fmt.Println("val is ", ele)
+		tmp := idx
+		arr[idx] = &tmp
+	}
+	for _, ele := range arr {
+		fmt.Println("val is ", *ele)
+	}
+}
+
+func Test_FromTopToBottom(t *testing.T) {
+	fmt.Println(coinChangeFromTop([]int{2}, 3))
 }
