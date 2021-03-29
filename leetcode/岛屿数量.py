649e9b3a32ef6606d9f73e95a6036f2f31ed9d04
hushichang <hscxrzs@gmail.com>
2021-03-29 21:56:14 +0800

    feat: review island nums

--- leetcode/岛屿数量.py
+++ leetcode/岛屿数量.py
@@ -1,26 +1,28 @@
 class Solution:
     def numIslands(self, grid):
         def dfs(grid, x, y):
-            if x < 0 or x >= len(grid) \
-                    or y < 0 or y >= len(grid[0]) \
-                    or grid[x][y] == '0':
-                return 0
+            if x < 0 or x >= len(grid) or \
+                    y < 0 or y >= len(grid[0]) or \
+                    grid[x][y] == '0':
+                return
             grid[x][y] = '0'
-            return 1 + \
-                   dfs(grid, x - 1, y) + \
-                   dfs(grid, x + 1, y) + \
-                   dfs(grid, x, y + 1) + \
-                   dfs(grid, x, y - 1)
+            dfs(grid, x, y - 1)
+            dfs(grid, x, y + 1)
+            dfs(grid, x - 1, y)
+            dfs(grid, x + 1, y)
 
-        res = 0
+        num = 0
         for x in range(len(grid)):
             for y in range(len(grid[0])):
-                res = max(res, dfs(grid, x, y))
-        return res
+                if grid[x][y] == '1':
+                    num += 1
+                    dfs(grid, x, y)
+        return num
 
 
-print(Solution().numIslands(
-    [["1", "1", "0", "0", "0"],
-     ["1", "1", "0", "0", "0"],
-     ["0", "0", "1", "1", "0"],
-     ["0", "0", "1", "1", "1"]]))
+if __name__ == '__main__':
+    print(Solution().numIslands(
+        [["1", "1", "0", "0", "0"],
+         ["1", "1", "1", "0", "0"],
+         ["0", "0", "1", "1", "0"],
+         ["0", "0", "1", "1", "1"]]))
