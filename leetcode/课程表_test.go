873d150b48bb35c220ab0412d2a6c72a7ee5d4bc
hushichang <hscxrzs@gmail.com>
2021-05-18 09:36:30 +0800

    fix: func name error

--- leetcode/课程表_test.go
+++ leetcode/课程表_test.go
@@ -7,5 +7,5 @@ import (
 
 func TestCanFinish(t *testing.T) {
 	prerequisites := [][]int{{0, 1}, {1, 2}, {2, 3}, {3, 4}}
-	fmt.Println(canFinishDfs(5, prerequisites))
+	fmt.Println(canFinishBFS(5, prerequisites))
 }
