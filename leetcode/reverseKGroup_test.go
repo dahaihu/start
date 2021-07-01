bc1dd43b9861db082a72a070789bf4fdf626d486
hushichang <hscxrzs@gmail.com>
2021-07-01 09:03:14 +0800

    feat: review reverse k group

--- leetcode/reverseKGroup_test.go
+++ leetcode/reverseKGroup_test.go
@@ -8,7 +8,10 @@ import (
 func TestReverseKGroup(t *testing.T) {
 	one, two, three, four, five, six, seven := &ListNode{Val:1},
 	&ListNode{Val:2},
-	&ListNode{Val:3},&ListNode{Val:4},&ListNode{Val:5},&ListNode{Val:6},
+	&ListNode{Val:3},
+	&ListNode{Val:4},
+	&ListNode{Val:5},
+	&ListNode{Val:6},
 	&ListNode{Val:7}
 	one.Next = two
 	two.Next = three
@@ -18,7 +21,7 @@ func TestReverseKGroup(t *testing.T) {
 	six.Next = seven
 
 
-	res := reverseKGroup(one, 5)
+	res := reverseKGroup(one, 2)
 	for res != nil {
 		fmt.Printf("%d -> ", res.Val)
 		res = res.Next
