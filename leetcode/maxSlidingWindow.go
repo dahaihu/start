3614d9b29aea5824db0ed053d045ba1f034f09a4
hushichang <hsccrzs@gmail.com>
2021-07-11 00:59:50 -0700

    feat: better max sliding window

--- leetcode/maxSlidingWindow.go
+++ leetcode/maxSlidingWindow.go
@@ -1,19 +1,27 @@
 package leetcode
 
 func maxSlidingWindow(nums []int, k int) []int {
-	window := make([]int, 0)
+	if len(nums) <= 1 {
+		return nums
+	}
+	window := make([]int, 0, k)
+	for i := 0; i < k; i++ {
+		for len(window) > 0 && window[len(window)-1] < nums[i] {
+			window = window[:len(window)-1]
+		}
+		window = append(window, nums[i])
+	}
 	result := make([]int, 0, len(nums)-k+1)
-	for idx, num := range nums {
-		if windowBegin := idx - k; windowBegin >= 0 && nums[windowBegin] == window[0] {
+	result = append(result, window[0])
+	for idx, num := range nums[k:] {
+		if len(window) > 0 && window[0] == nums[idx] {
 			window = window[1:]
 		}
-		for len(window) != 0 && window[len(window)-1] < num {
+		for len(window) > 0 && window[len(window)-1] < num {
 			window = window[:len(window)-1]
 		}
 		window = append(window, num)
-		if idx >= k-1 {
-			result = append(result, window[0])
-		}
+		result = append(result, window[0])
 	}
 	return result
 }
--- leetcode/maxSlidingWindow_test.go
+++ leetcode/maxSlidingWindow_test.go
@@ -8,6 +8,20 @@ import (
 func TestMaxSlidingWindow(t *testing.T) {
 	//nums := []int{1,3,1,2,0,5}
 	//nums := []int{1,3,1,2,0,5}
+	k := 3
 	nums := []int{7, -8, 7, 5, 7, 1, 6, 0}
-	fmt.Println(maxSlidingWindow(nums, 4))
+	fmt.Println(maxSlidingWindow(nums, k))
+	for i := k; i <= len(nums);i++ {
+		fmt.Print(maxSlice(nums[i-k:i]))
+	}
+}
+
+func maxSlice(nums []int) int {
+	m := nums[0]
+	for _, num :=range nums {
+		if num > m {
+			m = num
+		}
+	}
+	return m
 }
