1a3e9b47720e748e4215dc68016eea1edb8d1718
hushichang <hsccrzs@gmail.com>
2021-09-04 19:39:09 -0700

    feat: revie kth smallest

--- leetcode/有序矩阵中第K小的元素_test.go
+++ leetcode/有序矩阵中第K小的元素_test.go
@@ -6,14 +6,22 @@ import (
 )
 
 func TestCheck(t *testing.T) {
-	fmt.Println(check([][]int{
-		{1, 5, 9},
-		{10, 11, 13},
-		{12, 13, 15},
-	}, 13, 10, 3))
+	//fmt.Println(check([][]int{
+	//	{1, 5, 9},
+	//	{10, 11, 13},
+	//	{12, 13, 15},
+	//}, 13, 10, 3))
 	fmt.Println(kthSmallest([][]int{
 		{1, 5, 9},
 		{10, 11, 13},
 		{12, 13, 15},
-	}, 1))
+	}, 5))
+}
+
+func TestLsNum(t *testing.T) {
+	fmt.Println(leNum([][]int{
+		{1, 5, 9},
+		{10, 11, 13},
+		{12, 13, 15},
+	}, 11, 3))
 }
