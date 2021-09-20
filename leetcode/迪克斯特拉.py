48608ae04cff03a69768465b107b15127175fa32
hushichang <hscxrzs@gmail.com>
2021-09-20 20:14:01 +0800

    feat: review kijkstra

--- leetcode/迪克斯特拉.py
+++ leetcode/迪克斯特拉.py
@@ -1,9 +1,30 @@
 def dijkstra(graph):
-    pass
+    costs, processed = dict(), set()
+    processed.add('start')
+    costs['start'] = 0
+    for node, count in graph['start'].items():
+        costs[node] = count
+    node = find_lowest_cost_node(costs, processed)
+    while node is not None:
+        for next_node, count in graph.get(node, dict()).items():
+            next_cost = costs[node] + count
+            if next_cost < costs.get(next_node, float('inf')):
+                costs[next_node] = next_cost
+        processed.add(node)
+        node = find_lowest_cost_node(costs, processed)
+    return costs
 
 
 def find_lowest_cost_node(costs, processed):
-    pass
+    lowest_cost = float('inf')
+    lowest_node = None
+    for node, cost in costs.items():
+        if node in processed:
+            continue
+        if cost < lowest_cost:
+            lowest_cost = cost
+            lowest_node = node
+    return lowest_node
 
 
 if __name__ == '__main__':
