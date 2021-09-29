b85982ae3e6d79c2668d632fae76e8e50e86c2e2
hushichang <hscxrzs@gmail.com>
2021-09-29 14:54:49 +0800

    feat: binary tree print

--- awesomeProject.go
+++ awesomeProject.go
@@ -1,296 +1,321 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-
-type AVLNode struct {
-	Left, Right *AVLNode    // 表示指向左孩子和右孩子
-	Data        interface{} // 结点存储数据
-	height      int         // 记录这个结点此时的高度
+type Node struct {
+	Data   int
+	Left   *Node
+	Right  *Node
+	Bf     int
+	Parent *Node
 }
 
-func(avlNode *AVLNode) String() string {
-	if avlNode == nil {
-		return ""
+func buildTree(node *Node) (
+	box []string, boxLength, rootStart, rootEnd int,
+) {
+	if node == nil {
+		return nil, 0, 0, 0
 	}
-	format := `
-	%d
 
-%s		%s
-`
-	return fmt.Sprintf(format,
-		avlNode.Data.(int),
-		avlNode.Left,
-		avlNode.Right)
-}
+	rootStr := strconv.Itoa(node.Data)
+	rootWidth := len(rootStr)
+	gapSize := len(rootStr)
 
-// 定义comparer 指针类型
-// 用来比较两个结点中Data的大小
-type comparator func(a, b interface{}) int
-
-// compare 指针
-var compare comparator
-
-// 新建一个结点
-func NewAVLNode(data interface{}) *AVLNode {
-	return &AVLNode{
-		Left:   nil,
-		Right:  nil,
-		Data:   data,
-		// default leaf node height is 1
-		height: 1,
+	leftBox, leftBoxLength, leftRootStart, leftRootEnd := buildTree(node.Left)
+	rightBox, rightBoxLength, rightRootStart, rightRootEnd := buildTree(node.Right)
+	var (
+		line1, line2 []string
+	)
+	if leftBoxLength > 0 {
+		leftRootIndex := (leftRootStart+leftRootEnd)/2 + 1
+		line1 = append(line1, strings.Repeat(" ", leftRootIndex+1))
+		line1 = append(line1, strings.Repeat("_", leftBoxLength-leftRootIndex))
+		line2 = append(line2, strings.Repeat(" ", leftRootIndex)+"/")
+		line2 = append(line2, strings.Repeat(" ", leftBoxLength-leftRootIndex))
+		rootStart = leftBoxLength + 1
+		gapSize++
+	} else {
+		rootStart = 0
 	}
-}
-
-// 调整AVL树的平衡
-func (avlNode *AVLNode) adjust() *AVLNode {
-	// 如果右子树的高度比左子树的高度大于2
-	if avlNode.Right.GetHeight() - avlNode.Left.GetHeight() == 2 {
-		// 如果 avlNode.Right 的右子树的高度比avlNode.Right的左子树高度大
-		// 直接对avlNode进行左旋转
-		// 否则先对 avlNode.Right进行右旋转然后再对avlNode进行左旋转
-		if avlNode.Right.Right.GetHeight() > avlNode.Right.Left.GetHeight() {
-			avlNode = avlNode.LeftRotate()
-		}else{
-			avlNode = avlNode.RightThenLeftRotate()
+	line1 = append(line1, rootStr)
+	line2 = append(line2, strings.Repeat(" ", rootWidth))
+	if rightBoxLength > 0 {
+		rightRootIndex := (rightRootStart + rightRootEnd) / 2
+		line1 = append(line1, strings.Repeat("_", rightRootIndex))
+		line1 = append(line1, strings.Repeat(" ",
+			rightBoxLength-rightRootIndex+1))
+		line2 = append(line2, strings.Repeat(" ", rightRootIndex)+"\\")
+		line2 = append(line2, strings.Repeat(" ",
+			rightBoxLength-rightRootIndex))
+		gapSize++
+	}
+	rootEnd = rootStart + rootWidth - 1
+	gapStr := strings.Repeat(" ", gapSize)
+	newBox := []string{strings.Join(line1, ""), strings.Join(line2, "")}
+	childHeight := len(leftBox)
+	if rightBoxHeight := len(rightBox); rightBoxHeight > childHeight {
+		childHeight = rightBoxHeight
+	}
+	for i := 0; i < childHeight; i++ {
+		var lline, rline string
+		if i < len(leftBox) {
+			lline = leftBox[i]
+		} else {
+			lline = strings.Repeat(" ", leftBoxLength)
 		}
-		// 如果左子树的高度比右子树的高度大2
-	}else if avlNode.Right.GetHeight() - avlNode.Left.GetHeight() == -2 {
-		// 如果avlNode.Left的左子树高度大于avlNode.Left的右子树高度
-		// 那么就直接对avlNode进行右旋
-		// 否则先对avlNode.Left进行左旋，然后对avlNode进行右旋
-		if avlNode.Left.Left.GetHeight() > avlNode.Left.Right.GetHeight() {
-			avlNode = avlNode.RightRotate()
-		}else {
-			avlNode = avlNode.LeftThenRightRotate()
+		if i < len(rightBox) {
+			rline = rightBox[i]
+		} else {
+			rline = strings.Repeat(" ", rightBoxLength)
 		}
+		newBox = append(newBox, lline+gapStr+rline)
 	}
-
-	return avlNode
+	return newBox, len(newBox[0]), rootStart, rootEnd
 }
 
-// 顺时针旋转，右旋
-func (avlNode *AVLNode) RightRotate() *AVLNode {
-	headNode := avlNode.Left
-	avlNode.Left = headNode.Right
-	headNode.Right = avlNode
-
-	// 更新旋转后结点的高度
-	avlNode.height = Max(avlNode.Left.GetHeight(), avlNode.Right.GetHeight())+1
-	headNode.height = Max(headNode.Left.GetHeight(), headNode.Right.GetHeight())+1
-
-	return headNode
+//平衡二叉树，也叫 AVL 树（平衡二叉树作者的名字首字母），是自平衡的二叉查找树，
+//要求每个节点的左子树和右子树的高度差至多等于 1，
+//这个高度（深度）差的值叫做平衡因子 BF，也就是说 BF 的值不能大于1，
+//距离插入节点最近的，且平衡因子绝对值大于 1 的节点为根的子树，叫做最小不平衡子树，
+//一旦出现最小不平衡子树时，就进行左旋、右旋或双旋处理，以保持自身始终平衡
+//算法复杂度：O(logn)
+type AvlTree struct {
+	Tree *Node
 }
 
-// 先逆时针旋转再顺时针旋转，先左旋，在右旋
-func (avlNode *AVLNode) LeftThenRightRotate() *AVLNode {
-	// 先把左孩子结点进行左旋
-	avlNode.Left = avlNode.Left.LeftRotate()
-	// 然后把自己右旋
-	return avlNode.RightRotate()
-}
+const (
+	LH = 1
+	EH = 0
+	RH = -1
+)
 
-// 逆时针旋转，左旋
-func (avlNode *AVLNode) LeftRotate() *AVLNode {
-	headNode := avlNode.Right
-	avlNode.Right = headNode.Left
-	headNode.Left = avlNode
-
-	// 更新结点的高度
-	// 这里应该注意的俄式应该先更新avlNode 的高度，因为headNode结点在avlNode结点的上面
-	// headNode计算高度的时候要根据avlNode的高度来计算
-	avlNode.height = Max(avlNode.Left.GetHeight(), avlNode.Right.GetHeight())+1
-	headNode.height = Max(headNode.Left.GetHeight(), headNode.Right.GetHeight())+1
-
-	return headNode
-}
-
-// 先顺时针旋转再逆时针旋转，先右旋，再左旋
-func (avlNode *AVLNode) RightThenLeftRotate() *AVLNode {
-	// 先把右孩子进行右旋
-	avlNode.Right = avlNode.Right.RightRotate()
-	// 然后把自己右旋
-	return avlNode.LeftRotate()
-}
-
-// 新建AVL 树
-func NewAVLTree(data interface{}, myfunc comparator) (*AVLNode, error) {
-	if data == nil && myfunc == nil {
-		return nil, errors.New("不能为空")
-	}
-	compare = myfunc
-	return NewAVLNode(data), nil
-}
-
-// 获取结点数据
-func (avlNode *AVLNode) GetData() interface{} {
-	return avlNode.Data
-}
-
-// 设置结点数据
-func (avlNode *AVLNode) SetData(data interface{}) {
-	if avlNode == nil {
+//中序遍历
+func (t *AvlTree) MidOrderTraverse(tree *Node) {
+	if tree == nil {
 		return
 	}
-	avlNode.Data = data
+
+	t.MidOrderTraverse(tree.Left)
+	// fmt.Println(tree.Data)
+	fmt.Println(tree)
+	t.MidOrderTraverse(tree.Right)
 }
 
-// 获取结点的右孩子结点
-func (avlNode *AVLNode) GetRight() *AVLNode {
-	if avlNode == nil {
-		return nil
-	}
-	return avlNode.Right
+func (t *AvlTree) Insert(data int) {
+	t.InsertNode(data)
+	t.Reset() //将指针恢复到整棵树的根节点处
 }
 
-// 获取结点的左孩子结点
-func (avlNode *AVLNode) GetLeft() *AVLNode {
-	if avlNode == nil {
-		return nil
+func (t *AvlTree) Reset() {
+	tree := t.Tree
+	for tree.Parent != nil {
+		tree = tree.Parent
 	}
-	return avlNode.Left
+	t.Tree = tree
 }
 
-// 获取结点的高度
-func (avlNode *AVLNode) GetHeight() int {
-	if avlNode == nil {
-		return 0
+func (t *AvlTree) InsertNode(data int) bool {
+	if t.Tree == nil {
+		t.Tree = &Node{Data: data, Bf: EH}
+		return true
 	}
-	return avlNode.height
-}
+	tree := t.Tree
+	if data < tree.Data {
+		t.Tree = tree.Left
+		if !t.InsertNode(data) {
+			return false
+		} else {
+			if t.Tree.Parent == nil {
+				t.Tree.Parent = tree
+			}
+			if tree.Left == nil {
+				tree.Left = t.Tree
+			}
 
-//比较两个子树高度的大小
-func Max(a, b int) int {
-	if a >= b {
-		return a
-	} else {
-		return b
-	}
-}
-
-// 查找指定值
-func (avlNode *AVLNode) Find(data interface{}) *AVLNode {
-	var finded *AVLNode
-	// 调用比较函数比较两个结点的指的大小
-	switch compare(data, avlNode.Data) {
-	case -1:
-		finded = avlNode.Left.Find(data)
-	case 1:
-		finded = avlNode.Right.Find(data)
-	case 0:
-		return avlNode
-	}
-
-	return finded
-}
-
-// 查找最大值
-func (avlNode *AVLNode) FindMax() *AVLNode {
-	var finded *AVLNode
-
-	if avlNode.Right != nil {
-		finded = avlNode.Right.FindMin()
-	} else {
-		finded = avlNode
-	}
-
-	return finded
-}
-
-// 查找最小值
-func (avlNode *AVLNode) FindMin() *AVLNode { // 递归写法
-	var finded *AVLNode
-
-	if avlNode.Left != nil {
-		finded = avlNode.Left.FindMin()
-	} else {
-		finded = avlNode
-	}
-
-	return finded
-}
-
-// 插入数据
-// 因为没有定义 结点的parent指针，所以你插入数据就只能递归的插入，因为我要调整树的平衡和高度
-func (avlNode *AVLNode) Insert(value interface{}) *AVLNode {
-	if avlNode == nil {
-		return NewAVLNode(value)
-	}
-
-	switch compare(value, avlNode.Data) {
-	case -1:
-		// 如果value 小于 avlNode.Data 那么就在avlNode的左子树上去插入value
-		avlNode.Left = avlNode.Left.Insert(value)
-		avlNode = avlNode.adjust() // 自动调整平衡
-	case 1:
-		avlNode.Right = avlNode.Right.Insert(value)
-		avlNode = avlNode.adjust()
-	case 0:
-		fmt.Print("数据已经存在")
-	}
-	// 修改结点的高度
-	avlNode.height = Max(avlNode.Left.GetHeight(), avlNode.Right.GetHeight()) + 1
-
-	return avlNode
-}
-
-
-// 删除数据
-func (avlNode *AVLNode) Delete(value interface{}) *AVLNode {
-	// 没有找到匹配的数据
-	if avlNode == nil {
-		//fmt.Println("不存在", value)
-		return nil
-	}
-
-	switch compare(value, avlNode.Data) {
-	case 1:
-		avlNode.Right = avlNode.Right.Delete(value)
-	case -1:
-		avlNode.Left = avlNode.Left.Delete(value)
-	case 0:
-		// 找到数据，删除结点
-		if avlNode.Left != nil && avlNode.Right != nil { // 结点有左孩子和右孩子
-			avlNode.Data = avlNode.Right.FindMin().Data
-			avlNode.Right = avlNode.Right.Delete(avlNode.Data)
-		} else if avlNode.Left != nil { // 结点只有左孩子，无右孩子
-			avlNode = avlNode.Left
-		} else { // 结点只有右孩子或者无孩子
-			avlNode = avlNode.Right
+			switch tree.Bf {
+			case LH:
+				t.LeftBalance(tree)
+				return false
+			case EH:
+				tree.Bf = LH
+				t.Tree = tree
+				return true
+			case RH:
+				tree.Bf = EH
+				return false
+			}
 		}
+	} else if data > tree.Data {
+		t.Tree = tree.Right
+		if !t.InsertNode(data) {
+			return false
+		} else {
+			if t.Tree.Parent == nil {
+				t.Tree.Parent = tree
+			}
+			if tree.Right == nil {
+				tree.Right = t.Tree
+			}
 
+			switch tree.Bf {
+			case RH:
+				t.RightBalance(tree)
+				return false
+			case EH:
+				tree.Bf = RH
+				t.Tree = tree
+				return true
+			case LH:
+				tree.Bf = EH
+				return false
+			}
+		}
 	}
+	return true
+}
 
-	// 自动调整平衡, 如果avlNode!=nil说明执行了对avlNode 的某个子树执行了删除结点，那么就需要重新调整树的平衡
-	if avlNode != nil {
-		avlNode.height = Max(avlNode.Left.GetHeight(), avlNode.Right.GetHeight()) + 1
-		avlNode = avlNode.adjust() // 自动平衡
+func (t *AvlTree) LeftBalance(tree *Node) {
+	subTree := tree.Left
+	if subTree != nil {
+		switch subTree.Bf {
+		case LH:
+			// 新插入节点在左子节点的左子树上要做右单旋处理
+			tree.Bf = EH
+			subTree.Bf = EH
+			t.RightRotate(tree)
+		case RH:
+			// 新插入节点在左子节点的右子树上要做双旋处理
+			subTree_r := subTree.Right
+			if subTree_r != nil {
+				switch subTree_r.Bf {
+				case LH:
+					tree.Bf = RH
+					subTree.Bf = EH
+				case RH:
+					tree.Bf = EH
+					subTree.Bf = LH
+				}
+				subTree_r.Bf = EH
+				t.LeftRotate(subTree)
+				t.RightRotate(tree)
+			}
+
+		}
 	}
+}
 
-	return avlNode
+func (t *AvlTree) RightBalance(tree *Node) {
+	subTree := tree.Right
+	if subTree != nil {
+		switch subTree.Bf {
+		case RH:
+			//新插入节点在右子节点的右子树上要做左单旋处理
+			tree.Bf = EH
+			subTree.Bf = EH
+			t.LeftRotate(tree)
+		case LH:
+			//新插入节点在右子节点的左子树上要做双旋处理
+			subTree_l := subTree.Left
+			if subTree_l != nil {
+				switch subTree_l.Bf {
+				case LH:
+					tree.Bf = EH
+					subTree.Bf = RH
+				case RH:
+					tree.Bf = LH
+					subTree.Bf = EH
+				}
+				subTree_l.Bf = EH
+				t.RightRotate(subTree)
+				t.LeftRotate(tree)
+			}
+
+		}
+	}
+}
+
+//右单旋
+func (t *AvlTree) RightRotate(tree *Node) {
+	subTree := tree.Left
+	isLeft := false
+	if tree.Parent != nil {
+		subTree.Parent = tree.Parent //更新新子树的父节点
+		if tree.Parent.Left == tree {
+			isLeft = true
+		}
+	} else {
+		subTree.Parent = nil
+	}
+	tree.Left = subTree.Right //原来左节点的右子树挂到老的根节点的左子树
+	if subTree.Right != nil {
+		subTree.Right.Parent = tree
+	}
+	tree.Parent = subTree //原来的左节点变成老的根节点的父节点
+	subTree.Right = tree  //原来的根节点变成原来左节点的右子树
+	tree = subTree
+	if tree.Parent == nil { //旋转的是整棵树的根节点
+		t.Tree = tree
+	} else {
+		if isLeft { //更新老的子树根节点父节点指针指向新的根节点
+			tree.Parent.Left = tree
+		} else {
+			tree.Parent.Right = tree
+		}
+	}
+}
+
+//左单旋
+func (t *AvlTree) LeftRotate(tree *Node) {
+	subTree := tree.Right
+	isLeft := false
+	if tree.Parent != nil {
+		subTree.Parent = tree.Parent
+		if tree.Parent.Left == tree {
+			isLeft = true
+		}
+	} else {
+		subTree.Parent = nil
+	}
+	tree.Right = subTree.Left
+	if subTree.Left != nil {
+		subTree.Left.Parent = tree
+	}
+	tree.Parent = subTree
+	subTree.Left = tree
+	tree = subTree
+	if tree.Parent == nil {
+		t.Tree = tree
+	} else {
+		if isLeft {
+			tree.Parent.Left = tree
+		} else {
+			tree.Parent.Right = tree
+		}
+	}
 }
 
 func main() {
-	var comp func(interface{}, interface{}) int
-	comp = func(a, b interface{}) int {
-		aInt := a.(int)
-		bInt := b.(int)
-		if aInt > bInt {
-			return -1
-		} else if aInt < bInt {
-			return 1
-		} else {
-			return 0
-		}
-	}
+	avlTree := &AvlTree{}
 
-	tree, _ := NewAVLTree(1, comp)
-	for i := 2; i <= 5; i++ {
-		tree = tree.Insert(i)
+	//右单旋测试
+	avlTree.Insert(1)
+	avlTree.Insert(2)
+	avlTree.Insert(3)
+	avlTree.Insert(4)
+	avlTree.Insert(5)
+	avlTree.Insert(6)
+	avlTree.Insert(7)
+	box, _ , _, _ := buildTree(avlTree.Tree)
+	for _, line := range box {
+		fmt.Println(line)
 	}
-	fmt.Println(tree.String())
-
+	//avlTree.Insert(66)
+	//avlTree.Insert(83)
+	//avlTree.Insert(110)
+	//avlTree.Insert(130)
+	//fmt.Println(avlTree)
 }
