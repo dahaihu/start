05c1918a2d3576228bced8e8b641b2ea6c7b6dae
hushichang <hushichang@sensetime.com>
2022-03-03 21:08:46 +0800

    feat: studying 跳跃游戏

---
+++ leetcode/跳跃游戏_test.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_jump(t *testing.T) {
+	fmt.Println(jump([]int{2, 3, 1, 1, 4}))
+	fmt.Println(jump([]int{2, 1}))
+}
+
+func Test_canJump(t *testing.T) {
+	fmt.Println(canJump([]int{3, 2, 1, 0, 1}) == false)
+	fmt.Println(canJump([]int{3, 2, 1, 1, 1}) == true)
+}
