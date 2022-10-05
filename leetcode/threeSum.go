2075b881c6be21f2a7dd4b732901eeded1eb63d8
hushichang <hushichang@sensetime.com>
2022-10-05 13:56:17 +0800

    feat: review three sum

--- leetcode/threeSum.go
+++ leetcode/threeSum.go
@@ -5,25 +5,27 @@ import "sort"
 func threeSum(nums []int) [][]int {
 	sort.Slice(nums, func(i, j int) bool { return nums[i] <= nums[j] })
 	var result [][]int
-	for start := 0; start < len(nums)-2; start++ {
-		if start > 0 && nums[start] == nums[start-1] {
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
-		left, right := start+1, len(nums)-1
+		left, right := i+1, len(nums)-1
 		for left < right {
-			if cur := nums[start] + nums[left] + nums[right]; cur == 0 {
-				result = append(result, []int{nums[start], nums[left], nums[right]})
+			cur := nums[i] + nums[left] + nums[right]
+			switch {
+			case cur == 0:
+				result = append(result, []int{nums[i], nums[left], nums[right]})
 				left += 1
-				for nums[left] == nums[left-1] && left < right {
+				for left < right && nums[left] == nums[left-1] {
 					left += 1
 				}
 				right -= 1
-				for nums[right] == nums[right+1] && left < right {
+				for left < right && nums[right] == nums[right+1] {
 					right -= 1
 				}
-			} else if cur > 0 {
+			case cur > 0:
 				right -= 1
-			} else {
+			case cur < 0:
 				left += 1
 			}
 		}
