b5c14eec7b68e21978492c3aa961a25212e736dc
hushichang <hushichang@sensetime.com>
2022-10-06 18:31:17 +0800

    feat: review merge

--- leetcode/合并区间.go
+++ leetcode/合并区间.go
@@ -19,25 +19,74 @@ package leetcode
 链接：https://leetcode-cn.com/problems/merge-intervals
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
+
 import (
 	"sort"
 )
 
-func merge(intervals [][]int) [][]int {
+type Interval []int
+
+func (i Interval) First() int {
+	return i[0]
+}
+
+func (i Interval) Last() int {
+	return i[1]
+}
+
+func (i Interval) SetFirst(first int) {
+	i[0] = first
+}
+
+func (i Interval) SetLast(last int) {
+	i[1] = last
+}
+
+type Intervals []Interval
+
+func (is Intervals) Len() int {
+	return len(is)
+}
+
+func (is Intervals) Less(i, j int) bool {
+	switch {
+	case is[i].First() == is[j].First():
+		return is[i].Last() <= is[j].Last()
+	default:
+		return is[i].First() < is[j].First()
+	}
+}
+
+func (is Intervals) Swap(i, j int) {
+	is[i], is[j] = is[j], is[i]
+}
+
+func NewIntervals(intervals [][]int) Intervals {
+	out := make(Intervals, 0, len(intervals))
+	for _, interval := range intervals {
+		out = append(out, NewInterval(interval))
+	}
+	return out
+}
+
+func NewInterval(interval []int) Interval {
+	return Interval{interval[0], interval[1]}
+}
+
+func merge(intervals Intervals) Intervals {
 	if len(intervals) <= 1 {
 		return intervals
 	}
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] <= intervals[j][0]
-	})
-	result := [][]int{intervals[0]}
+	sort.Sort(intervals)
+	result := make(Intervals, 1)
+	result[0] = intervals[0]
 	for i := 1; i < len(intervals); i++ {
-		last := result[len(result)-1]
-		cur := intervals[i]
-		if last[1] < cur[0] {
+		pre, cur := result[len(result)-1], intervals[i]
+		switch {
+		case pre.Last() < cur.First():
 			result = append(result, cur)
-		} else if cur[1] > last[1] {
-			last[1] = cur[1]
+		case pre.Last() < cur.Last():
+			pre.SetLast(cur.Last())
 		}
 	}
 	return result
--- leetcode/合并区间_test.go
+++ leetcode/合并区间_test.go
@@ -2,10 +2,17 @@ package leetcode
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
 func TestMerge(t *testing.T) {
-	fmt.Println(merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}))
-	fmt.Println(merge([][]int{{1, 4}, {2, 3}}))
+	fmt.Println(merge(NewIntervals([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}})))
+	fmt.Println(merge(NewIntervals([][]int{{1, 4}, {2, 3}})))
+}
+
+func TestSort(t *testing.T) {
+	values := []int{3, 1, 4, 1, 10}
+	sort.Slice(values, func(i, j int) bool { return values[i] <= values[j] })
+	fmt.Println(values)
 }
